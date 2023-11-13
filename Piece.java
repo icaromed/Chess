65b7e8f341d38d698edce2c55906a8d4de4c396b
icaro <icaromeddev@gmail.com>
2023-11-13 18:13:35 -0300

    Constructor for initiating piece at a given board position, added getters for this position, using the row/ columns coordinate Added a method that moves the piece to the given position

--- Piece.java
+++ Piece.java
@@ -1,2 +1,28 @@
 public class Piece {
+    // stores the piece's position on the board
+    private int row;
+    private int column;
+
+    public Piece(int row, int column){
+        // starts the piece on it's inicial position
+        // on the board
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public boolean move(int row, int column){
+        // moves the piece to the given position
+        this.row = row;
+        this.column = column;
+        return true;
+    }
+
 }
